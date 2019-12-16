7545e0e1fa7f524013740f13b44109b87914fd57
moddz <idaniel@qq.com>
2019-12-16 16:57:36 +0800

    enter

--- js/index.js
+++ js/index.js
@@ -1,15 +1,1 @@
-/**
- * BirdWhisperer
- * by 2gua
- */
-var BirdWhisperer = /** @class */ (function () {
-    function BirdWhisperer(message) {
-        this.chirping = message;
-    }
-    BirdWhisperer.prototype.chirp = function () {
-        return "Ah~ oh~ " + this.chirping;
-    };
-    return BirdWhisperer;
-}());
-var birdWhisperer = new BirdWhisperer("coo-coo-coo...");
-document.body.innerHTML = birdWhisperer.chirp();
+alert("hello world");
