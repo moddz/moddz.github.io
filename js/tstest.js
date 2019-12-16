1c334b339809eed95371ce1738404241478b0971
moddz <idaniel@qq.com>
2019-12-16 17:11:39 +0800

    enter

---
+++ js/tstest.js
@@ -0,0 +1,16 @@
+/**
+ * BirdWhisperer
+ * by 2gua
+ */
+var BirdWhisperer = /** @class */ (function() {
+  function BirdWhisperer(message) {
+    this.chirping = message;
+  }
+  BirdWhisperer.prototype.chirp = function() {
+    return "Ah~ oh~ " + this.chirping;
+  };
+  return BirdWhisperer;
+})();
+var birdWhisperer = new BirdWhisperer("moddz coo-coo-coo...");
+document.body.innerHTML = birdWhisperer.chirp();
+//# sourceMappingURL=tstest.js.map
