ab38bb1f27c330082640b227dd2b80074d3809a7
moddz <idaniel@qq.com>
2019-12-20 16:38:00 +0800

    enter

--- js/index.js
+++ js/index.js
@@ -1,3 +1,9 @@
 const age = 14;
 
 age < 15 ? console.log("you are so young") : console.log("you are old enough");
+
+function mofa() {
+  message = "hello my friends!";
+}
+mofa();
+console.log(message);
