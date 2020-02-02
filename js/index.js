c27ca47509095a2bbc24fa85713701e5d5b882b7
moddz <idaniel@qq.com>
2020-02-02 23:35:01 +0800

    enter

--- js/index.js
+++ js/index.js
@@ -1,8 +1,3 @@
-var message; // 这个变量声明之后默认取得了 undefined 值
-
-// 下面这个变量并没有声明 // var age
-
-alert(typeof message);
-alert(typeof age);
-
-// "undefined" // "undefined"
+var num1 = 25;
+var num2 = -num1 - 1;
+alert(num2);
