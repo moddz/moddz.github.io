a68298459936e6df92b302ce0fa6bd41f8804828
moddz <idaniel@qq.com>
2020-03-03 13:35:57 +0800

    enter

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,9 @@
-print('hello world')
+print('The game of python first try')
+temp = input('Please think about which Number I thought')
+guess = int(temp)
+if guess == 8:
+    print('Shit,you are sooo fucking cool')
+    print('emmm,there is nothing price!')
+else:
+    print('Sorry,you are whong')
+print('Game over')
