12145b6b237ae970ad4f0edb0bb76fdb7f5a1743
moddz <idaniel@qq.com>
2020-07-07 11:57:20 +0800

    enter

--- js/tstest.js
+++ js/tstest.js
@@ -2,11 +2,11 @@
  * BirdWhisperer
  * by 2gua
  */
-var BirdWhisperer = /** @class */ (function() {
+var BirdWhisperer = /** @class */ (function () {
   function BirdWhisperer(message) {
     this.chirping = message;
   }
-  BirdWhisperer.prototype.chirp = function() {
+  BirdWhisperer.prototype.chirp = function () {
     return "Ah~ oh~ " + this.chirping;
   };
   return BirdWhisperer;
